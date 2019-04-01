a820d74db147dedfd70cf3cb457924ba1276a6af
Lukas Tillmann <lukas.tillmann@orf.at>
2019-04-01 15:21:50 +0200

    adds install scripts for nvm and tpm (tmux plugin manager)

---
+++ install_nvm.sh
@@ -0,0 +1,3 @@
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+echo 'source ~/.bashrc'
+echo 'nvm install node to install latest version'
---
+++ install_tpm.sh
@@ -0,0 +1,2 @@
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+echo 'check https://github.com/tmux-plugins/tpm for further instructions'
