1405425135e5a2302d96f741d777ea2bca28296c
Lukas Tillmann <lukas.tillmann@orf.at>
2019-04-09 12:15:50 +0200

    adds comments to tpm install script

--- install_tpm.sh
+++ install_tpm.sh
@@ -1,2 +1,5 @@
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-echo 'check https://github.com/tmux-plugins/tpm for further instructions'
+echo "check https://github.com/tmux-plugins/tpm for further instructions"
+echo "Install plugins:"
+echo "Add new plugin to ~/.tmux.conf with set -g @plugin '...'"
+echo "Press prefix + I (capital i, as in Install) to fetch the plugin."
