4d27e007f3d0538765e2da565f778646887adfa0
Lukas Tillmann <lukas.tillmann@orf.at>
2023-06-20 08:19:38 +0200

    feat: add nvim-regexplainer plugin

---
+++ nvim-config/lua/plugins/nvim-regexplainer.lua
@@ -0,0 +1,8 @@
+return { 
+    'bennypowers/nvim-regexplainer',
+     config = function() require'regexplainer'.setup() end,
+     requires = {
+        'nvim-treesitter/nvim-treesitter',
+        'MunifTanjim/nui.nvim',
+     } 
+}
