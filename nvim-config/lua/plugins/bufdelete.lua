fff4e643bb44520728eff106bfa8f6b58ffb3a82
Lukas Tillmann <lukas.tillmann@orf.at>
2023-07-05 08:22:16 +0200

    add bufdelete plugin + keymaps

---
+++ nvim-config/lua/plugins/bufdelete.lua
@@ -0,0 +1,20 @@
+return {
+	"famiu/bufdelete.nvim",
+	keys = {
+		{ "<leader>q", ":Bdelete<cr>", desc = "Delete buffer without changing window layout" },
+		{ "<leader>Q", ":Bdelete!<cr>", desc = "Force delete buffer without changing window layout" },
+		{ "<leader>0", ":%bd|e#|bd#<cr>", desc = "Close all buffers except current" },
+		{
+			"<leader>i",
+			":BufferLineCyclePrev<cr>",
+			desc = "Cycle through buffers left. (Mnemonic: overlaps with jumplist navigation <C-i>)",
+		},
+		{
+			"<S-tab>",
+			":BufferLineCycleNext<cr>",
+			desc = "Cycle through buffers right. (Mnemonic: overlaps with jumplist navigation <C-o>)",
+		},
+		{ "<leader>I", ":BufferLineMovePrev<cr>", desc = "Move current buffer to the left in the bufferline" },
+		{ "<leader>O", ":BufferLineMoveNext<cr>", desc = "Move current buffer to the right in the bufferline" },
+	},
+}
