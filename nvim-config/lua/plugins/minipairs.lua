5ad7320755b157a693c4250ffda5e61c6e926a35
Lukas Tillmann <lukas.tillmann@orf.at>
2023-08-29 08:17:23 +0200

    disable auto-pairs before non-space characters (minipairs)

--- nvim-config/lua/plugins/minipairs.lua
+++ nvim-config/lua/plugins/minipairs.lua
@@ -1,8 +1,24 @@
 -- auto pairs
 return {
-	"echasnovski/mini.pairs",
-	event = "VeryLazy",
-	config = function(_, opts)
-		require("mini.pairs").setup(opts)
-	end,
+    "echasnovski/mini.pairs",
+    event = "VeryLazy",
+    opts = {
+        mappings = {
+            -- disable if there is an adjacent non-space character
+            ["`"] = {
+                action = "closeopen",
+                pair = "``",
+                neigh_pattern = "[^%S][^%S]",
+                register = { cr = false }
+            },
+            ["("] = {
+                action = "open",
+                pair = "()",
+                neigh_pattern = "[^%S][^%S]",
+            }
+        }
+    },
+    config = function(_, opts)
+        require("mini.pairs").setup(opts)
+    end,
 }
