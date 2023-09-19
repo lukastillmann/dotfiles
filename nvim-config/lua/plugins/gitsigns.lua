c3e87b545c16abaeb5c334d7ec491e6be68d600c
Lukas Tillmann <lukas.tillmann@orf.at>
2023-09-19 10:24:02 +0200

    fix gitsigns opts

--- nvim-config/lua/plugins/gitsigns.lua
+++ nvim-config/lua/plugins/gitsigns.lua
@@ -1,3 +1,19 @@
 return {
-    "lewis6991/gitsigns.nvim"
+    "lewis6991/gitsigns.nvim",
+    opts = {
+        signs              = {
+            add          = { text = '+' },
+            change       = { text = '~' },
+            delete       = { text = '_' },
+            topdelete    = { text = '‾' },
+            changedelete = { text = '~-' },
+            untracked    = { text = '┆' },
+        },
+        signcolumn         = true,  -- Toggle with `:Gitsigns toggle_signs`
+        numhl              = false, -- Toggle with `:Gitsigns toggle_numhl`
+        linehl             = false, -- Toggle with `:Gitsigns toggle_linehl`
+        word_diff          = true,  -- Toggle with `:Gitsigns toggle_word_diff`
+        current_line_blame = true,  -- Toggle with `:Gitsigns toggle_current_line_blame`
+        --      current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+    }
 }
