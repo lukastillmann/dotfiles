92a9c072b39f8314f584163aa489d9341e3c1fad
Lukas Tillmann <lukas.tillmann@orf.at>
2024-11-29 13:35:31 +0100

    rename fugitive plugin filename

---
+++ nvim-config/lua/plugins/fugitive.lua
@@ -0,0 +1,14 @@
+---
+--- fugitive.vim: A Git wrapper so awesome, it should be illegal
+---
+--- source: https://github.com/tpope/vim-fugitive
+---
+return {
+    "tpope/vim-fugitive",
+    config = function()
+        -- wslview is a utility to allow opening urls in the windows browser
+        -- necessary for GBrowse
+        -- https://wslutiliti.es/wslu/man/wslview.html
+        vim.g.netrw_browsex_viewer = "wslview"
+    end
+}
