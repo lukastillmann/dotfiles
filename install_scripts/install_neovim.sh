e0ec19eda278f292e78df7ed24f9ccbc7818c3bc
Lukas Tillmann <lukas.tillmann@orf.at>
2024-11-29 13:31:02 +0100

    update install_neovim script

--- install_scripts/install_neovim.sh
+++ install_scripts/install_neovim.sh
@@ -30,34 +30,10 @@ dep_ch "gcc" "cmake" || true
 
 CURRENT_DIR="$(pwd)"
 NEOVIM_DIR="$HOME/.config/nvim/neovim"
+BUILD_TYPE="RelWithDebInfo" # install latest nightly release
 
 #checks if neovim directory already exists
 [ ! -d "$NEOVIM_DIR" ] || rm -rf "$NEOVIM_DIR"
-
-# make a choice for build type
-inf "" "Please choose a build type"
-PS3='Please enter your choice: '
-options=("Nightly" "Release (Default)" "exit script")
-select opt in "${options[@]}"; do
-	case $opt in
-	"Nightly")
-		BUILD_TYPE="RelWithDebInfo"
-		break
-		;;
-	"Release (Default)")
-		BUILD_TYPE="Release"
-		break
-		;;
-	"exit script")
-		exit 0
-		;;
-	*)
-		BUILD_TYPE="Release"
-		inf "[OK]" "Build type set to Release"
-		break
-		;;
-	esac
-done
 
 #cloning latest neovim from official repo
 git clone https://github.com/neovim/neovim.git "$NEOVIM_DIR"
