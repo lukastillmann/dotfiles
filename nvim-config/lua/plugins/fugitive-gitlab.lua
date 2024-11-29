569663d3e0bc63c5f3ea728e708198f14f8a8df9
Lukas Tillmann <lukas.tillmann@orf.at>
2024-11-29 13:35:13 +0100

    add token to fugitive-gitlab

--- nvim-config/lua/plugins/fugitive-gitlab.lua
+++ nvim-config/lua/plugins/fugitive-gitlab.lua
@@ -2,5 +2,7 @@ return {
     "shumphrey/fugitive-gitlab.vim",
     config = function()
         vim.g.fugitive_gitlab_domains = { "https://git.orf.apa.net" }
+        local token = os.getenv("GITLAB_TOKEN")
+        vim.g.gitlab_api_keys = { ["git.orf.apa.net"] = token }
     end
 }
