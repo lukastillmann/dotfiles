da6029b2a5ef0ac6edb46730cb352fc35701f572
Lukas Tillmann <lukas.tillmann@orf.at>
2024-11-29 13:32:26 +0100

    remove vim-prettier

--- nvim-config/lua/plugins/prettier.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "prettier/vim-prettier",
-    build = "npm install",
-    config = function()
-        vim.api.nvim_set_keymap("n", "<F3>", "<cmd>Prettier<cr>", {
-            desc = "Prettier"
-        })
-    end,
-}
