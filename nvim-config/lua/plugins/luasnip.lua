28efebd117b9409b9f5c7f468628b9670fc19e75
Lukas Tillmann <lukas.tillmann@orf.at>
2024-11-29 13:31:40 +0100

    get correct luasnip snipets

--- nvim-config/lua/plugins/luasnip.lua
+++ nvim-config/lua/plugins/luasnip.lua
@@ -13,12 +13,28 @@ return {
     dependencies = {
         "rafamadriz/friendly-snippets",
     },
+    opts = {
+        -- see https://github.com/L3MON4D3/LuaSnip/blob/master/DOC.md#lua
+        snip_env = {
+            s = function(...)
+                local snip = require("luasnip").s(...)
+                table.insert(getfenv(2).ls_file_snippets, snip)
+            end,
+            parse = function(...)
+                local snip = require("luasnip").parser.parse_snippet(...)
+                table.insert(getfenv(2).ls_file_snippets, snip)
+            end,
+            -- Add any other environment-specific functions or variables here
+        },
+    },
     config = function()
         -- Ensure LuaSnip is configured correctly
         require("luasnip").config.set_config({
             history = true,
             updateevents = "TextChanged,TextChangedI",
         })
+
+        require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/lua/snippets" })
 
         -- Load friendly-snippets
         require("luasnip.loaders.from_vscode").lazy_load()
