954a409df2e69ec724ff6a6471affab3a8892a39
Lukas Tillmann <lukas.tillmann@orf.at>
2025-01-14 06:34:33 +0100

    add ability to toggle format_on_save with hotkey

--- nvim-config/lua/plugins/conform.lua
+++ nvim-config/lua/plugins/conform.lua
@@ -1,3 +1,14 @@
+local format_on_save_enabled = true
+
+local toggle_format_on_save = function()
+    format_on_save_enabled = not format_on_save_enabled
+    if format_on_save_enabled then
+        print("Format on Save: Enabled")
+    else
+        print("Format on Save: Disabled")
+    end
+end
+
 return {
     'stevearc/conform.nvim',
     config = function()
@@ -10,29 +21,50 @@ return {
                 javascriptreact = { "prettier", lsp_format = "never" },
                 typescriptreact = { "prettier", lsp_format = "never" },
                 vue = { "prettier", lsp_format = "never" },
-                json = { "prettier" },
+                json = { "prettier", lsp_format = "fallback" },
                 yaml = { "prettier" },
                 markdown = { "prettier" },
                 html = { "prettier" },
                 css = { "prettier" },
                 scss = { "prettier" },
             },
-            format_on_save = {
-                timeout_ms = 1000,
-                lsp_format = "fallback" -- use language server as fallback
+            format_on_save = function(_)
+                if format_on_save_enabled then
+                    return {
+                        timeout_ms = 2000,
+                        lsp_format = "fallback", -- use language server as fallback
+                    }
+                end
+            end,
+        xformat_on_save = {
+                timeout_ms = 2000,
+                lsp_format = "fallback",          -- use language server as fallback
+                callback = function()
+                    return format_on_save_enabled -- Checks if format_on_save is enabled
+                end
             },
+            -- Conform will notify you when a formatter errors
+            notify_on_error = true,
+            -- Conform will notify you when no formatters are available for the buffer
+            notify_no_formatters = true,
             formatters = {
                 -- the following config is not needed currently, the prettier config is read correctly.
                 -- I'll leave it in as backup for now
-                -- prettier = {
-                --     command = "prettier",
-                --     args = { "--stdin-filepath", "$FILENAME" },
-                --     condition = function()
-                --         -- this makes sure, the prettier config is read correctly
-                --         return vim.fn.filereadable(".prettierrc") == 1 or vim.fn.filereadable(".prettierrc.json") == 1 or
-                --             vim.fn.filereadable(".prettierrc.js") == 1
-                --     end,
-                -- },
+                prettier = {
+                    command = "prettier",
+                    args = { "--stdin-filepath", "$FILENAME" },
+                    condition = function(_, ctx)
+                        -- do not format markdown files in gp.nvim chats saved  in
+                        -- ~/.local/share/nvim/gp/chats/ (see gp.lua)
+                        if (string.find(ctx.filename, "/nvim/gp/chats")) then
+                            return false
+                        end
+
+                        -- this makes sure, the prettier config is read correctly
+                        return vim.fn.filereadable(".prettierrc") == 1 or vim.fn.filereadable(".prettierrc.json") == 1 or
+                            vim.fn.filereadable(".prettierrc.js") == 1
+                    end,
+                },
                 -- prettierd = {
                 --     command = "prettierd",
                 --     args = { "--stdin-filepath", "$FILENAME" },
@@ -43,7 +75,7 @@ return {
                 --     end,
                 -- },
             },
-            -- log_level = vim.log.levels.TRACE
+            log_level = vim.log.levels.TRACE
         })
 
         vim.keymap.set("n", "<F3>", function()
@@ -53,5 +85,7 @@ return {
         vim.keymap.set("n", "<leader>p", function()
             conform.format({ async = true, lsp_format = "fallback" })
         end, { desc = "Format with Conform and LSP Fallback" })
+
+        vim.keymap.set("n", "<S-F3>", toggle_format_on_save, { desc = "Toggle Format on Save" })
     end
 }
