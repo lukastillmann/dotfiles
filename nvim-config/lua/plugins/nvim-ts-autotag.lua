3e395356d4a4f4bb6faaa2293950d0761a6bf33b
Lukas Tillmann <lukas.tillmann@orf.at>
2025-04-25 13:30:23 +0200

    add nvim-ts-autotag plugin

---
+++ nvim-config/lua/plugins/nvim-ts-autotag.lua
@@ -0,0 +1,18 @@
+-------------------------------------------------------------------
+--- Use treesitter to auto close and auto rename html tag
+---
+--- Source: https://github.com/windwp/nvim-ts-autotag
+-------------------------------------------------------------------
+return {
+    "windwp/nvim-ts-autotag",
+    config = function()
+        require('nvim-ts-autotag').setup({
+            opts = {
+                -- Defaults
+                enable_close = true,         -- Auto close tags
+                enable_rename = true,        -- Auto rename pairs of tags
+                enable_close_on_slash = true -- Auto close on trailing </
+            },
+        })
+    end
+}
