bc7025518c926f5de22ef766144124e807bbe304
Lukas Tillmann <lukas.tillmann@orf.at>
2025-04-25 13:29:20 +0200

    fix for macos

--- nvim-config/lua/plugins/fugitive.lua
+++ nvim-config/lua/plugins/fugitive.lua
@@ -7,8 +7,8 @@ return {
     "tpope/vim-fugitive",
     config = function()
         -- wslview is a utility to allow opening urls in the windows browser
-        -- necessary for GBrowse
+        -- necessary for GBrowse on wsl
         -- https://wslutiliti.es/wslu/man/wslview.html
-        vim.g.netrw_browsex_viewer = "wslview"
+        -- vim.g.netrw_browsex_viewer = "wslview"
     end
 }
