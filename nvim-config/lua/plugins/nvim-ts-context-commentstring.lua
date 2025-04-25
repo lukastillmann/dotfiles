838928655228378c26c0583b334e45b117690f78
Lukas Tillmann <lukas.tillmann@orf.at>
2025-04-25 13:30:18 +0200

    add comment

--- nvim-config/lua/plugins/nvim-ts-context-commentstring.lua
+++ nvim-config/lua/plugins/nvim-ts-context-commentstring.lua
@@ -1,1 +1,5 @@
+----------------------------------------------
+-- Neovim treesitter plugin for setting the commentstring based on the cursor location in a file.
+----------------------------------------------
+
 return { "JoosepAlviste/nvim-ts-context-commentstring", lazy = true }
