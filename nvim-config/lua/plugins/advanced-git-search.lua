a26a821c503d0b3c7e9b931ec66d93e35cd65626
Lukas Tillmann <lukas.tillmann@orf.at>
2025-04-25 13:26:35 +0200

    add advanced-git-search plugin

--- nvim-config/lua/plugins/advanced-git-search.lua
+++ nvim-config/lua/plugins/advanced-git-search.lua
@@ -1,3 +1,21 @@
+----------------------------------------------------------
+-- Search your git history by commit message, content and author in Neovim
+--
+-- Source: https://github.com/aaronhallaert/advanced-git-search.nvim
+----------------------------------------------------------
+--
+-- 📖 Open a picker
+-- :AdvancedGitSearch or :AdvancedGitSearch {command}
+--
+-- 🔎 Enter a query
+--
+-- Your usual search experience. See the individual commands for the grep behaviour.
+--
+-- ✏️ Further search on commit author with @
+--
+-- The prompt is split on @. Everything following the @ is the pattern for the author name.
+--
+
 return {
     "aaronhallaert/advanced-git-search.nvim",
     requires = { "nvim-telescope/telescope.nvim", "tpope/vim-fugitive" },
