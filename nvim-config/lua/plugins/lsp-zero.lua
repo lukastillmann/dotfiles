854b7989c35144245fb994a7610b5807d689b215
Lukas Tillmann <lukas.tillmann@orf.at>
2025-07-21 13:45:36 +0200

    remove code action keymap (conflict)

--- nvim-config/lua/plugins/lsp-zero.lua
+++ nvim-config/lua/plugins/lsp-zero.lua
@@ -25,7 +25,7 @@ return {
             vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
             -- vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
             vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-            vim.keymap.set('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+            -- vim.keymap.set('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
         end
 
         lsp_zero.extend_lspconfig({
