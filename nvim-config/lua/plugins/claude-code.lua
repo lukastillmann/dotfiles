b581f008682a8fbeccb96a1ccb58897616f74aba
Lukas Tillmann <lukas.tillmann@orf.at>
2025-07-21 13:44:41 +0200

    add claude-code plugin

---
+++ nvim-config/lua/plugins/claude-code.lua
@@ -0,0 +1,22 @@
+return {
+    "greggh/claude-code.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim", -- Required for git operations
+    },
+    config = function()
+        local opts = {
+            window = {
+                position = "float",
+                float = {
+                    width = "90%",     -- Take up 90% of the editor width
+                    height = "90%",    -- Take up 90% of the editor height
+                    row = "center",    -- Center vertically
+                    col = "center",    -- Center horizontally
+                    relative = "editor",
+                    border = "double", -- Use double border style
+                },
+            }
+        }
+        require("claude-code").setup(opts)
+    end
+}
