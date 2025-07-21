09d2706e0a03b03925ba08b4b067eacbb4dd06a8
Lukas Tillmann <lukas.tillmann@orf.at>
2025-07-21 13:47:12 +0200

    more minimal minimal_init.lua

--- nvim-config/minimal_init.lua
+++ nvim-config/minimal_init.lua
@@ -1,57 +1,35 @@
--- this template is borrowed from nvim-lspconfig
-local on_windows = vim.loop.os_uname().version:match("Windows")
+-- Set leader key
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
 
-local function join_paths(...)
-    local path_sep = on_windows and "\\" or "/"
-    local result = table.concat({ ... }, path_sep)
-    return result
-end
+-- Sensible settings
+vim.opt.number = true             -- Show line numbers
+vim.opt.mouse = "a"               -- Enable mouse support
+vim.opt.clipboard = "unnamedplus" -- Use system clipboard
+vim.opt.expandtab = true          -- Convert tabs to spaces
+vim.opt.shiftwidth = 4            -- Indentation width
+vim.opt.tabstop = 4               -- Tab width
+vim.opt.softtabstop = 4           -- Spaces per tab in insert mode
+vim.opt.smartindent = true        -- Enable smart indentation
+vim.opt.autoindent = true        -- Enable smart indentation
+vim.opt.ai = true        -- Enable smart indentation
+vim.opt.wrap = false              -- Disable line wrapping
+vim.opt.swapfile = false          -- Disable swap files
+vim.opt.backup = false            -- Disable backups
+vim.opt.undofile = true           -- Enable persistent undo
+vim.opt.ignorecase = true         -- Case-insensitive search...
+vim.opt.showmatch = true
+vim.opt.splitbelow = true
+vim.opt.smartcase = true          -- ...unless search contains uppercase
+vim.opt.hlsearch = true           -- Highlight search results
+vim.opt.incsearch = true          -- Show search matches while typing
+vim.opt.scrolloff = 8             -- Keep 8 lines visible above/below cursor
+vim.opt.sidescrolloff = 8         -- Keep 8 columns visible left/right of cursor
+vim.opt.cursorline = true
+vim.opt.termguicolors = true      -- Enable true color support
+vim.opt.signcolumn = "yes"        -- Always show sign column to avoid shifting
+vim.opt.wildmenu = true
+vim.opt.foldmethod = "indent"  -- Fold based on indentation
+vim.opt.foldenable = false     -- Don't fold by default, open files normally
 
-vim.g.loaded_remote_plugins = ""
-vim.cmd([[set runtimepath=$VIMRUNTIME]])
-
-local temp_dir = vim.loop.os_getenv("TEMP") or "/tmp"
-
-vim.cmd("set packpath=" .. join_paths(temp_dir, "nvim", "site"))
-
-local package_root = join_paths(temp_dir, "nvim", "site", "pack")
-local install_path = join_paths(package_root, "packer", "start", "packer.nvim")
-local compile_path = join_paths(install_path, "plugin", "packer_compiled.lua")
-
-local null_ls_config = function()
-    local null_ls = require("null-ls")
-    -- add only what you need to reproduce your issue
-    null_ls.setup({
-        sources = {
-            null_ls.builtins.formatting.eslint 
-        },
-        debug = true,
-    })
-end
-
-local function load_plugins()
-    -- only add other plugins if they are necessary to reproduce the issue
-    require("packer").startup({
-        {
-            "wbthomason/packer.nvim",
-            {
-                "jose-elias-alvarez/null-ls.nvim",
-                requires = { "nvim-lua/plenary.nvim" },
-                config = null_ls_config,
-            },
-        },
-        config = {
-            package_root = package_root,
-            compile_path = compile_path,
-        },
-    })
-end
-
-if vim.fn.isdirectory(install_path) == 0 then
-    vim.fn.system({ "git", "clone", "https://github.com/wbthomason/packer.nvim", install_path })
-    load_plugins()
-    require("packer").sync()
-else
-    load_plugins()
-    require("packer").sync()
-end
+vim.cmd("colorscheme habamax")
