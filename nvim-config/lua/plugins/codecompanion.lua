0e8084782840a308c9a13ba7872735642b9eb247
Lukas Tillmann <lukas.tillmann@orf.at>
2025-09-05 10:47:44 +0200

    update codecompanion config

--- nvim-config/lua/plugins/codecompanion.lua
+++ nvim-config/lua/plugins/codecompanion.lua
@@ -14,40 +14,9 @@ return {
         vim.cmd([[cab cc CodeCompanion]])
 
         require("codecompanion").setup({
-            adapters = {
-                anthropic = function()
-                    return require("codecompanion.adapters").extend("anthropic", {})
-                end,
-                qwen = function()
-                    return require("codecompanion.adapters").extend("ollama", {
-                        name = "qwen",
-                        schema = {
-                            model = {
-                                default = "qwen2.5-coder:14b-adapted",
-                            },
-                        },
-                    })
-                end,
-                openai = function()
-                    return require("codecompanion.adapters").extend("openai", {
-                        name = "openai",
-                        schema = {
-                            model = {
-                                default = "gpt-4o"
-                            },
-                            api_key = {
-                                default = os.getenv("OPENAI_API_KEY")
-                            }
-                        }
-                    })
-                end
-            },
             strategies = {
                 chat = {
                     adapter = "anthropic",
-                    opts = {
-                        completion_provider = "cmp"
-                    }
                 },
                 inline = {
                     adapter = "anthropic",
@@ -56,11 +25,25 @@ return {
                     adapter = "anthropic",
                 },
             },
+            adapters = {
+                acp = {
+                    gemini_cli = function()
+                        return require("codecompanion.adapters").extend("gemini_cli", {
+                            env = {
+                                api_key = "GEMINI_API_KEY",
+                            },
+                        })
+                    end,
+                },
+            },
             display = {
                 diff = {
                     enabled = true,
                     provider = "mini_diff"
                 }
+            },
+            opts = {
+                log_level = "INFO",                   -- TRACE|DEBUG|ERROR|INFO
             },
             extensions = {
                 mcphub = {
