77b9f07a6cf116ae4bbf0c2e16ba9d8904e49280
Lukas Tillmann <lukas.tillmann@orf.at>
2025-09-05 14:55:55 +0200

    update lsp code comments

--- nvim-config/lua/core/lsp.lua
+++ nvim-config/lua/core/lsp.lua
@@ -1,27 +1,16 @@
--- Get the correct location for typescript library
--- local os_name = vim.loop.os_uname().sysname
--- local tspath
---
--- -- linux
--- if os_name == "Linux" then
---     tspath = "/home/lukas/.local/share/nvim/mason/packages/vue-language-server/node_modules/typescript/lib/"
---     -- macOS
--- elseif os_name == "Darwin" then
---     tspath = "/Users/lukas/.local/share/nvim/mason/packages/vue-language-server/node_modules/typescript/lib/"
--- end
-
 local default_keymaps = {
-    { keys = "K",          func = vim.lsp.buf.hover,       desc = "Documentation",   has = "hoverProvider" },
-    { keys = "gd",         func = vim.lsp.buf.definition,  desc = "Goto Definition", has = "definitionProvider" },
-    { keys = "<leader>ca", func = vim.lsp.buf.code_action, desc = "Code Actions" },
-    { keys = "<F4>",       func = vim.lsp.buf.code_action, desc = "Code Actions" },
+    { keys = "K",    func = vim.lsp.buf.hover,       desc = "Documentation",   has = "hoverProvider" },
+    { keys = "gd",   func = vim.lsp.buf.definition,  desc = "Goto Definition", has = "definitionProvider" },
+    -- { keys = "<leader>ca", func = vim.lsp.buf.code_action, desc = "Code Actions" },
+    { keys = "<F4>", func = vim.lsp.buf.code_action, desc = "Code Actions" },
     -- { keys = "<leader>.",  func = vim.lsp.buf.code_action, desc = "Code Actions" },
     -- { keys = "<leader>cA", func = M.action.source,         desc = "Source Actions" },
-    { keys = "<F2>",       func = vim.lsp.buf.rename,      desc = "Code Rename" },
-    { keys = "K",          func = vim.lsp.buf.hover,       desc = "Documentation",   has = "hoverProvider" },
-    { keys = "gr",         func = vim.lsp.buf.references,  desc = "References" }
+    { keys = "<F2>", func = vim.lsp.buf.rename,      desc = "Code Rename" },
+    { keys = "K",    func = vim.lsp.buf.hover,       desc = "Documentation",   has = "hoverProvider" },
+    { keys = "gr",   func = vim.lsp.buf.references,  desc = "References" }
 }
 
+-- On Attach function - to get called when a language server is attached to a file
 local on_attach = function(client, bufnr)
     for _, keymap in ipairs(default_keymaps) do
         if not keymap.has or client.server_capabilities[keymap.has] then
@@ -52,6 +41,7 @@ local vue_language_server_path = vim.fn.stdpath('data') ..
     "/mason/packages/vue-language-server/node_modules/@vue/language-server"
 local tsserver_filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' }
 
+-- typescript vue plugin
 local vue_plugin = {
     name = '@vue/typescript-plugin',
     location = vue_language_server_path,
@@ -59,6 +49,11 @@ local vue_plugin = {
     configNamespace = 'typescript',
 }
 
+-------------------------------
+--- Language Server Configs ---
+-------------------------------
+
+-- tsserver
 local ts_ls_config = {
     init_options = {
         plugins = {
@@ -68,6 +63,7 @@ local ts_ls_config = {
     filetypes = tsserver_filetypes,
 }
 
+-- lua_ls config
 local lua_ls_config = {
     settings = {
         Lua = {
@@ -81,12 +77,15 @@ local lua_ls_config = {
     }
 }
 
+-- vue_ls server
 local vue_ls_config = {}
 
+-- set configs
 vim.lsp.config("vue_ls", vue_ls_config)
 vim.lsp.config("ts_ls", ts_ls_config)
 vim.lsp.config("lua_ls", lua_ls_config)
 
+-- enable language servers
 vim.lsp.enable({
     "lua_ls",
     "ts_ls",
@@ -96,3 +95,16 @@ vim.lsp.enable({
     "cssls",
     "vue_ls",
 })
+
+-- NOT NEEDED ANY MORE
+-- Get the correct location for typescript library
+-- local os_name = vim.loop.os_uname().sysname
+-- local tspath
+--
+-- -- linux
+-- if os_name == "Linux" then
+--     tspath = "/home/lukas/.local/share/nvim/mason/packages/vue-language-server/node_modules/typescript/lib/"
+--     -- macOS
+-- elseif os_name == "Darwin" then
+--     tspath = "/Users/lukas/.local/share/nvim/mason/packages/vue-language-server/node_modules/typescript/lib/"
+-- end
