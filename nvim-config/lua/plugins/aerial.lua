991d519d999af43e657470750208d81690fef8e6
Lukas Tillmann <lukas.tillmann@orf.at>
2025-09-05 14:56:05 +0200

    re-add aerial plugin

--- nvim-config/lua/plugins/aerial.lua
+++ nvim-config/lua/plugins/aerial.lua
@@ -1,9 +1,15 @@
 return {
-	"stevearc/aerial.nvim",
-	opts = {},
-	-- Optional dependencies
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter",
-		"nvim-tree/nvim-web-devicons",
-	},
+    "stevearc/aerial.nvim",
+    opts = {},
+    -- Optional dependencies
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+        vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle<CR>")
+        vim.keymap.set("n", "<leader>A", "<cmd>AerialNavToggle<CR>")
+
+        require("aerial").setup()
+    end
 }
