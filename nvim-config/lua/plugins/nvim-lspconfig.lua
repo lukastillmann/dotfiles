b5f94711ac0bbe136985f688bfea33471305303d
Lukas Tillmann <lukas.tillmann@orf.at>
2025-09-05 10:47:28 +0200

    disable <leader>ca for code actions

--- nvim-config/lua/plugins/nvim-lspconfig.lua
+++ nvim-config/lua/plugins/nvim-lspconfig.lua
@@ -19,7 +19,7 @@ return {
                 vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
                 vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
                 vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-                vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+                -- vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
                 vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
             end,
         })
