ea9aea7d5a022b72845220798addfabbeeb479f8
Lukas Tillmann <lukas.tillmann@orf.at>
2026-02-13 06:16:11 +0100

    feat: update snacks.lua config

--- nvim-config/lua/plugins/snacks.lua
+++ nvim-config/lua/plugins/snacks.lua
@@ -27,7 +27,7 @@ return {
         words = { enabled = true },
         styles = {
             notification = {
-                -- wo = { wrap = true } -- Wrap notifications
+                wo = { wrap = true } -- Wrap notifications
             }
         },
     },
@@ -107,24 +107,24 @@ return {
         { "<c-_>",      function() Snacks.terminal() end,                                       desc = "which_key_ignore" },
         { "]]",         function() Snacks.words.jump(vim.v.count1) end,                         desc = "Next Reference",           mode = { "n", "t" } },
         { "[[",         function() Snacks.words.jump(-vim.v.count1) end,                        desc = "Prev Reference",           mode = { "n", "t" } },
-        {
-            "<leader>N",
-            desc = "Neovim News",
-            function()
-                Snacks.win({
-                    file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
-                    width = 0.6,
-                    height = 0.6,
-                    wo = {
-                        spell = false,
-                        wrap = false,
-                        signcolumn = "yes",
-                        statuscolumn = " ",
-                        conceallevel = 3,
-                    },
-                })
-            end,
-        }
+        -- {
+        --     "<leader>N",
+        --     desc = "Neovim News",
+        --     function()
+        --         Snacks.win({
+        --             file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
+        --             width = 0.6,
+        --             height = 0.6,
+        --             wo = {
+        --                 spell = false,
+        --                 wrap = false,
+        --                 signcolumn = "yes",
+        --                 statuscolumn = " ",
+        --                 conceallevel = 3,
+        --             },
+        --         })
+        --     end,
+        -- }
     },
     init = function()
         vim.api.nvim_create_autocmd("User", {
